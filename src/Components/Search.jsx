ccfa046c88530df7128a492080b3dfe2e15e199b
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-24 12:53:11 +0600

    debounce added

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import SearchImage from "../assets/search.svg";
+import { filterByTitle } from "../redux/filter/actions";
 
 const Search = () => {
+    const [input, setInput] = useState("");
+    const [timeOutId, setTimeOutId] = useState(null);
+    const dispatch = useDispatch();
+
+    const handleAuthorFilter = () => {
+        dispatch(filterByTitle(input));
+    };
+
+    const handleInput = (e) => {
+        setInput(e.target.value);
+        if (timeOutId) {
+            clearTimeout(timeOutId);
+        }
+        const tId = setTimeout(() => {
+            console.log("called");
+            handleAuthorFilter();
+        }, 1500);
+        setTimeOutId(tId);
+    };
+
     return (
         <div className="border mt-6 border-slate-200 flex items-center w-11/12 lg:w-1/2 mx-auto bg-gray-50 h-12 px-5 rounded-lg text-sm ring-emerald-200">
             <input
@@ -9,6 +31,8 @@ const Search = () => {
                 type="search"
                 name="search"
                 placeholder="Search"
+                value={input}
+                onChange={handleInput}
             />
             <img
                 className="inline h-6 cursor-pointer"
