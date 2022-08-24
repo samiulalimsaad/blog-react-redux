b4ad11bca0ca8c906fac92b0bd0d7562f54aee8a
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-24 14:09:16 +0600

    reseter added

--- src/redux/filter/actions.js
+++ src/redux/filter/actions.js
@@ -2,6 +2,10 @@ import {
     FILTER_BY_AUTHOR,
     FILTER_BY_CATEGORY,
     FILTER_BY_TITLE,
+    RESET_ALL,
+    RESET_BY_AUTHOR,
+    RESET_BY_CATEGORY,
+    RESET_BY_TITLE,
 } from "./actionType";
 
 export const filterByCategory = (category) => ({
@@ -17,4 +21,20 @@ export const filterByAuthor = (author) => ({
 export const filterByTitle = (title) => ({
     type: FILTER_BY_TITLE,
     payload: { title },
+});
+
+export const resetByCategory = () => ({
+    type: RESET_BY_CATEGORY,
+});
+
+export const resetByAuthor = () => ({
+    type: RESET_BY_AUTHOR,
+});
+
+export const resetByTitle = (title) => ({
+    type: RESET_BY_TITLE,
+});
+
+export const resetAll = (title) => ({
+    type: RESET_ALL,
 });
--- src/redux/filter/reducer.js
+++ src/redux/filter/reducer.js
@@ -2,6 +2,10 @@ import {
     FILTER_BY_AUTHOR,
     FILTER_BY_CATEGORY,
     FILTER_BY_TITLE,
+    RESET_ALL,
+    RESET_BY_AUTHOR,
+    RESET_BY_CATEGORY,
+    RESET_BY_TITLE,
 } from "./actionType";
 import { initialState } from "./initialState";
 
@@ -13,16 +17,33 @@ export const filterReducer = (state = initialState, action) => {
                 ...state,
                 author: payload.author,
             };
+        case RESET_BY_AUTHOR:
+            return {
+                ...state,
+                author: "",
+            };
         case FILTER_BY_CATEGORY:
             return {
                 ...state,
                 category: payload.category,
+            };
+        case RESET_BY_CATEGORY:
+            return {
+                ...state,
+                category: "",
             };
         case FILTER_BY_TITLE:
             return {
                 ...state,
                 title: payload.title,
             };
+        case RESET_BY_TITLE:
+            return {
+                ...state,
+                title: "",
+            };
+        case RESET_ALL:
+            return initialState;
 
         default:
             return state;
