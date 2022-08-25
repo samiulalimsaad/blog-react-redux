451dc8a908c9c5ba864920cb5589d768fa70b63b
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-25 18:40:58 +0600

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import logo from "../assets/lws.svg";
 
 const Navbar = () => {
     return (
         <nav className="bg-slate-100 shadow-md">
-            <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3 items-center">
-                <a href="index.html">
-                    <img className="h-10" src={logo} alt="Learn with Sumit" />
-                </a>
+            <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3 items-center text-3xl font-semibold">
+                <p>Blog</p>
             </div>
         </nav>
     );
